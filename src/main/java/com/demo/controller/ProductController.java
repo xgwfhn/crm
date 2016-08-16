c98f65ada4bfbaa4b0cb8db25b0fe092e883a7ed
xgwfhn@126.com <xgwfhn@126.com>
2016-08-16 15:47:24 +0800

    提交

--- src/main/java/com/demo/controller/ProductController.java
+++ src/main/java/com/demo/controller/ProductController.java
@@ -10,7 +10,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping(value = "/product")
@@ -22,6 +21,22 @@ public class ProductController {
 		return "index"; 
 	}
 	
+	/**跳转到多记录提交页面
+	 * @param request
+	 * @param model
+	 * @return
+	 */
+	@RequestMapping(value = "/recordsSubmit", method = RequestMethod.GET)
+	public String recordsSubmit(HttpServletRequest request, Model model) {
+		System.out.println("recordsSubmit");
+		return "recordsSubmit"; 
+	}
+	
+	/**测试json字符串
+	 * @param request
+	 * @param model
+	 * @return
+	 */
 	@RequestMapping(value = "/add", method = RequestMethod.POST)
 	@ResponseBody	//加上@ResponseBody,说明改方法不会跳转到页面,而是返回json字符串
 	public String add(HttpServletRequest request, Model model) {
@@ -29,12 +44,40 @@ public class ProductController {
 		return "{\"success\":\"true\"}"; 
 	}
 	
+	/**测试将map转为json字符串
+	 * @param request
+	 * @param model
+	 * @return
+	 */
 	@RequestMapping(value = "/add1", method = RequestMethod.POST)
-	@ResponseBody	//加上@ResponseBody,说明改方法不会跳转到页面,而是返回json字符串
+	@ResponseBody	//加上@ResponseBody,说明改方法不会跳转到页面,此处调用spring工具类 自动将map转为 json字符串   
 	public Map<String,Object> add1(HttpServletRequest request, Model model) {
 		System.out.println(request.getParameter("name"));//获取表单数据
 		Map<String,Object> map=new HashMap<String,Object>();
 		map.put("success", true);
 		return map; 
 	}
+	
+	
+	/**批量提交表格中的多条记录
+	 * @param request
+	 * @param model
+	 * @return
+	 */
+	@RequestMapping(value = "/batchSubmit", method = RequestMethod.POST)
+	@ResponseBody
+	public Map<String,Object> batchSubmit(HttpServletRequest request, Model model) {
+		String json = request.getParameter("dataJson");     
+		 JSONArray array =null;
+		 //= JSONArray.from;  
+		    Object[] obj = new Object[array.size()];  
+		    for (int i = 0; i < array.size(); i++) {  
+		        JSONObject jsonObject = array.getJSONObject(i);  
+		        obj[i] = JSONObject.toBean(jsonObject, clazz);  
+		    }  
+		Map<String,Object> map=new HashMap<String,Object>();
+		map.put("success", true);
+		return map; 
+	}
+	
 } 
