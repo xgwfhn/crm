6d80dc5403df2a35a30d497f3fa7a9b603c745ac
xgwfhn@126.com <xgwfhn@126.com>
2016-08-20 11:38:28 +0800

    qqq

--- src/main/java/com/demo/controller/ProductController.java
+++ src/main/java/com/demo/controller/ProductController.java
@@ -5,11 +5,17 @@ import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
+
+import org.apache.commons.lang.StringUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.demo.model.Product;
 
 @Controller
 @RequestMapping(value = "/product")
@@ -31,6 +37,29 @@ public class ProductController {
 		System.out.println("recordsSubmit");
 		return "recordsSubmit"; 
 	}
+	
+	/**跳转到多表单提交页面
+	 * @param request
+	 * @param model
+	 * @return
+	 */
+	@RequestMapping(value = "/jumpFormsSubmit", method = RequestMethod.GET)
+	public String jumpFormsSubmit(HttpServletRequest request, Model model) {
+		System.out.println("formsSubmit");
+		return "formsSubmit"; 
+	}
+	
+	
+	/**跳转到文件上传页面
+	 * @param request
+	 * @param model
+	 * @return
+	 */
+	@RequestMapping(value = "/fileUpload", method = RequestMethod.GET)
+	public String fileUpload(HttpServletRequest request, Model model) {
+		return "fileUpload";
+	}
+	
 	
 	/**测试json字符串
 	 * @param request
@@ -59,7 +88,7 @@ public class ProductController {
 	}
 	
 	
-	/**批量提交表格中的多条记录
+	/**批量提交表格中的多条记录,并将记录转为Object[]
 	 * @param request
 	 * @param model
 	 * @return
@@ -67,17 +96,41 @@ public class ProductController {
 	@RequestMapping(value = "/batchSubmit", method = RequestMethod.POST)
 	@ResponseBody
 	public Map<String,Object> batchSubmit(HttpServletRequest request, Model model) {
-		String json = request.getParameter("dataJson");     
-		 JSONArray array =null;
-		 //= JSONArray.from;  
-		    Object[] obj = new Object[array.size()];  
+		String json = request.getParameter("dataJson");   
+		System.out.println("json:"+json);
+		if(StringUtils.isNotEmpty(json)){//StringUtils为commons-lang下的工具类
+			JSONArray array =JSONArray.fromObject(json);
+		    Product[] objs = new Product[array.size()];   
 		    for (int i = 0; i < array.size(); i++) {  
 		        JSONObject jsonObject = array.getJSONObject(i);  
-		        obj[i] = JSONObject.toBean(jsonObject, clazz);  
-		    }  
-		Map<String,Object> map=new HashMap<String,Object>();
-		map.put("success", true);
-		return map; 
+		        objs[i] = (Product) JSONObject.toBean(jsonObject, Product.class);  
+		    } 
+		    for(Product pro:objs){
+		    	System.out.println(pro.getItemid());   
+		    }
+			Map<String,Object> map=new HashMap<String,Object>();
+			map.put("success", true);
+			map.put("datas", objs);
+			return map; 
+		}
+		return null;
 	}
+	
+	
+	/**多表单提交
+	 * @param request
+	 * @param model
+	 * @return
+	 */
+	@RequestMapping(value = "/formsSubmit", method = RequestMethod.POST)
+	@ResponseBody
+	public Map<String,Object> formsSubmit(HttpServletRequest request, Model model) {
+		String json = request.getParameter("dataJson");   
+		System.out.println("-----name:"+request.getParameter("name"));
+		System.out.println("-----json:"+json);
+		return null;
+	}
+	
+	
 	
 } 
